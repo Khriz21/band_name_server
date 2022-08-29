624cffd91d101dc5455555c411958fa92eca3cea
Cristian Juarez <khriz_flakitho21@outlook.com>
2022-08-29 22:45:55 +0200

    Fin seccion 3

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+require("dotenv").config();
+
+//? App de express
+const app = express();
+
+//* Node Server
+const server = require("http").createServer(app);
+module.exports.io = require("socket.io")(server);
+
+require('./sockets/socket');
+
+//? Mensaje de Sockets
+
+
+//? path público
+const publicPath = path.resolve(__dirname, "public");
+
+app.use(express.static(publicPath));
+
+server.listen(process.env.PORT, (err) => {
+  if (err) throw new Error("Err");
+  console.log(`Servidor corriendo en el puerto: `, process.env.PORT);
+});
